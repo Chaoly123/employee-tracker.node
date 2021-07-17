e934f752d428f127cd32f65c635e020eb1a7cfb7
Chao Ly <lychaos123@gmail.com>
2021-07-16 19:05:43 -0500

    add homework assigment into employee-tracker.node. Had do some codings with the file before moving it.

---
+++ employee.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS employee_DB;
+CREATE DATABASE employee_DB;
+
+USE employee_DB;
+
+CREATE TABLE department(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles(
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10,2),
+    department_id INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employees(
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    PRIMARY KEY (id)
+);
+
+
---
+++ index.js
@@ -0,0 +1,140 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "chaos123",
+    database: "employee_DB"
+})
+
+connection.connect(err => {
+    if (err) throw err;
+    init()
+})
+
+function init() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: "choice",
+            choices: ["Add Departments", "View Departments", "Add Roles", "View Roles", "Add Employees", "View Employees", "Done"],
+            message: "What would you like to do?"
+        }
+    ]).then(({ choice }) => {
+        if (choice == "Add Deppartments") {
+            addDepartments()
+        } else if (choice == "View Departments") {
+            viewDepartments(); {
+            } if (choice == "Add Roles") {
+                addRoles(); {
+                } if (choice == "View Roles") {
+                    viewRoles(); {
+                    } if (choice == "Add Employees") {
+                        addEmployees(); {
+                        } if (choice == "View Employees") {
+                        } else {
+                            console.log("bye")
+                            connection.end()
+                        }
+                    };
+                }
+
+                function addDeparments() {
+                    inquirer.prompt([
+                        {
+                            type: 'input',
+                            name: 'newDepartment',
+                            message: 'What is the new department name?'
+                        }
+                    ]).then(answer => {
+                        let queryString = `
+        INSERT INTO department(name)
+        VALUES (?)`
+
+                        connection.query(queryString, [answer.newDepartment], err => {
+                            if (err) throw err;
+                            init()
+                        })
+                    })
+                }
+
+                function addRoles() {
+                    inquirer.prompt([
+                        {
+                            type: 'input',
+                            name: 'newRoles',
+                            message: 'What is the new roles?'
+                        },
+                        {
+                            type: "input",
+                            name: "newSalary",
+                            message: "How much is the salary?"
+                        },
+                        {
+                            type: "list",
+                            name: "deparment",
+                            message: "Which department does this role go to?"
+                        }
+                    ]).then(answer => {
+                        let queryString = `
+        INSERT INTO roles(name)
+        VALUES (?)`
+
+                        connection.query(queryString, [answer.newRoles], err => {
+                            if (err) throw err;
+                            init()
+                        })
+                    })
+                }
+
+                function addEmployees() {
+                    inquirer.prompt([
+                        {
+                            type: 'input',
+                            name: 'firstName',
+                            message: 'What is the new employees first name?'
+                        },
+                        {
+                            type: "input",
+                            name: "secondName",
+                            message: "What is the new employees last name?"
+                        },
+                        {
+                            type: "input",
+                            name: "field",
+                            message: "What is the field of work?"
+                        },
+                    ]).then(answer => {
+                        let queryString = `
+        INSERT INTO employee(name)
+        VALUES (?)`
+
+                        connection.query(queryString, [answer.newEmployees], err => {
+                            if (err) throw err;
+                            init()
+                        })
+                    })
+                }
+
+                function viewDepartments() {
+                    let queryString = `
+    SELECT *
+    FROM department`
+
+                    connection.query(queryString, (err, data) => {
+                        if (err) throw err;
+
+                        console.log('\n')
+                        console.table(data)
+                        console.log('\n')
+
+                        init()
+                    })
+                }
+            }
+        }
+    }
+    )
+}
